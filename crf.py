5535ad7e77b109cb129b266cb127ee346a73c547
bencrabbe <benoit.crabbe@gmail.com>
2017-05-13 17:32:55 +0200

    added crf

--- crf.py
+++ crf.py
@@ -19,7 +19,7 @@ def make_dataset(text):
         tok_trigrams = list(zip(tokens,tokens[1:],tokens[2:]))
         tok_bigramsL = list(zip(tokens,tokens[1:]))
         tok_bigramsR = list(zip(tokens[1:],tokens))
-        dataset.append(tuple(pos,zip(tok_trigrams,tok_bigramsL,tok_bigramsR)))
+        dataset.append((pos,list(zip(tok_trigrams,tok_bigramsL,tok_bigramsR))))
                     
     return dataset
 
@@ -113,18 +113,18 @@ class LinearCRF:
         backward[N-1,:] = 1.0
 
         #recurrence
-        for i in range(N-1,0,-1):
+        for i in range(N-2,-1,-1):
             for j in range(K):
                 for succ in range(K):
                     backward[i,j] += backward[i+1,succ] * self.score(self.Y[j],self.Y[succ],sentence[i+1])
-        
-        return (backward,backward[0,:].sum())
+        Z = sum([self.score(self.source_tag,self.Y[succ],sentence[0]) *  backward[0,succ] for succ in range(K)])
+        return (backward,Z)
     
     def train(self,dataset,step_size=0.1,max_epochs=100):
         """
         @param dataset: a list of couples (y_tags,x_words)
         """
-        self.Y = list(set([y for y in ytags for (ytags,xwords) in dataset]))
+        self.Y = list(set([y for (ytags,xwords) in dataset for y in ytags]))
 
         #pre-computes delta_ref (first term of the gradient is constant)
         delta_ref  = SparseWeightVector()
@@ -140,21 +140,22 @@ class LinearCRF:
             delta_pred = SparseWeightVector()
 
             for ytags,xwords in dataset:
-                N = len(sentence)
+                N = len(xwords)
                 K = len(self.Y)
                 alphas,Z1  = self.forward(xwords)
                 betas, Z2  = self.backward(xwords) 
+                print(Z1,Z2)
                 assert(Z1==Z2)
                 Z = Z1
                 #init forward-backward
                 for ytag in range(K):
-                    prob = (self.score(self.source_tag,self.Y[ytag],xwords[0]) * beta[0,ytag]) / Z
+                    prob = (self.score(self.source_tag,self.Y[ytag],xwords[0]) * betas[0,ytag]) / Z
                     delta_pred += prob * SparseWeightVector.code_phi(xwords[0],(self.source_tag,self.Y[ytag]))
                 #forward-backward loop
                 for i in range(1,N):
                     for yprev in range(K):
                         for ytag in range(K):
-                            prob = (alpha[i-1,yprev] * self.score(self.Y[yprev],self.Y[ytag],xwords[i]) * beta[i,ytag]) / Z
+                            prob = (alphas[i-1,yprev] * self.score(self.Y[yprev],self.Y[ytag],xwords[i]) * betas[i,ytag]) / Z
                             delta_pred += prob * SparseWeightVector.code_phi(xwords[i],(self.Y[yprev],self.Y[ytag]))
             self.model += step_size*(delta_ref-delta_pred)
             print('Loss (log likelihood) = ',loss)
@@ -164,3 +165,4 @@ D = make_dataset(corpus)
 print(D)
 
 crf = LinearCRF()
+crf.train(D)
