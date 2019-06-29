4fe372350346b610f3f694e2240ae9597eb7e35c
bencrabbe <benoit.crabbe@gmail.com>
2019-06-29 19:49:13 +0200

    added kaggle data generator

--- data/imdb2kaggle.py
+++ data/imdb2kaggle.py
@@ -13,33 +13,50 @@ def load_dataset(dir_path,ref_label):
     dpath    = os.path.abspath(dir_path)
     data_set = [] 
     for f in os.listdir(dpath):
-        ordinal_value = f.split('.')[0].split('_')[1]
         filepath      = os.path.join(dpath,f)
         file_stream   = open(filepath)
         #normalize spaces and removes tabs
         text          = file_stream.read()
         file_stream.close()
-        data_set.append((text,ref_label,ordinal_value))
+        data_set.append((text,ref_label))
     return data_set
 
-testpos = load_dataset("aclImdb/test/pos","1")
-testneg = load_dataset("aclImdb/test/neg","0")
-test = testpos + testneg
-random.shuffle(test)
 
-#generate student test data
-ostream = open('sentimentIMDB_test.csv','w')
-print('idx,text',file=ostream)
-for idx,line in enumerate(test):
-    text, y1,y2 = line 
-    print('%d,%s'%(idx,text),file=ostream)
-ostream.close()
+def generate_train_data(filename,datalines):
+    ostream = open(filename,'w')
+    for idx,line in enumerate(datalines):
+        text,label  = line        
+        print(','.join([label,text]),file=ostream)
+    ostream.close()
 
-#generate gold data
-ostream = open('sentimentIMDB_gold.csv','w')
-print('idx,bool_sentiment,scaled_sentiment',file=ostream)
-for idx,line in enumerate(test):
-    text,y1,y2 = line
-    print(','.join([str(idx),y1,y2]),file=ostream)
-ostream.close()
+def generate_student_test(filename,datalines):
+    ostream = open('sentimentIMDB_test.csv','w')
+    print('idx,text',file=ostream)
+    for idx,line in enumerate(test):
+        text, label  = line 
+        print('%d,%s'%(idx,text),file=ostream)
+    ostream.close()
     
+def generate_kaggle_solution(filename,datalines):
+    ostream = open('sentimentIMDB_gold.csv','w')
+    print('idx,sentY',file=ostream)
+    for idx,line in enumerate(test):
+        text,y1  = line
+        print(','.join([str(idx),y1]),file=ostream)
+    ostream.close()
+
+if __name__ == '__main__':
+
+    trainpos = load_dataset("aclImdb/train/pos","1")
+    trainneg = load_dataset("aclImdb/train/neg","0")
+    train = trainpos + trainneg
+    random.shuffle(train)
+    generate_train_data('sentimentIMDB_train.csv',train)
+
+    testpos = load_dataset("aclImdb/test/pos","1")
+    testneg = load_dataset("aclImdb/test/neg","0")
+    test = testpos + testneg
+    random.shuffle(test)
+    
+    generate_student_test('sentimentIMDB_test.csv',test)
+    generate_kaggle_solution('sentimentIMDB_solution.csv',test)
